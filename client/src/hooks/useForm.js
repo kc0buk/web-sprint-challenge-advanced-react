40af55d93fb1ed1b17a8ac19bbd6509164499afa
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-08-07 09:20:20 -0700

    Added useForm.js to hooks. Updated CheckoutForm.js to useForm.js. Updated readme.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,17 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react'
+
+export const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue)
+
+    const handleChanges = event => {
+        const { name, value } = event.target
+
+        setValues({
+            ...values,
+            [name] : value
+        })
+    }
+
+    return [values, handleChanges]
+}
