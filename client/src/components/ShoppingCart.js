b6281cb9dea662d25a275f60820c19b0bbe1d8df
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-08-07 10:52:03 -0700

    Updated readme. Added ShoppingCard.test.js, and updated ShoppingCart.js to support testing.

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -7,7 +7,7 @@ export default function ShoppingCart(props) {
   return (
     <div className="cart">
       {props.cart.map((plant) => (
-        <div className="plant-card" key={plant.id}>
+        <div className="plant-card" key={plant.id} data-testid="plantCard">
           <img className="plant-image" src={plant.img} alt={plant.name} />
           <div className="plant-details">
             <h2 className="plant-name">{plant.name}</h2>
